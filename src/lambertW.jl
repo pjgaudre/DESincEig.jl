20433f0d6d5e1bff66a39dfc937cbf620a3d7b32
Philippe Gaudreau <pjgaudre@ualberta.ca>
2016-02-19 14:41:56 -0700

    Update lambertW.jl

--- src/lambertW.jl
+++ src/lambertW.jl
@@ -2,7 +2,7 @@ export lambertW
 
 #
 # The Lambert W function (See reference [4])
-# The following algorithm computes the Lambert-W function to 15 correct digits using Halley's method for any x
+# The following algorithm computes the Lambert-W function to machine accuracy using Halley's method for any x
 # in the domain I=( -1/exp(1), ∞ ). This function can also compute the lambert-W function component-wise for
 # vectors or matrices. The Lambert-W function is used in the module DESincEig to compute the mesh size h.
 # Input: x:: Number, Vector or Matrix
@@ -11,12 +11,10 @@ export lambertW
 function lambertW(x::Real)
     if x < -exp(-one(x))
         return throw(DomainError())
-    elseif x < -0.2576872774
-        w0 = -(5+2*e*x)/3 + sqrt(2*(e*x+one(x)))
-    elseif x ≤ 1.272507835
-        w0 = x-x^2
+    elseif x < 0
+        w0 = e*x/(one(x)+inv(inv(sqrt(2*e*x+2))+inv(e-one(x))-inv(sqrt(2))))
     else
-        w0 = log(x)-log(log(x))
+        w0 = log(one(x)+x)*(one(x)-log(one(x)+log(one(x)+x))/(2+log(one(x)+x)))
     end
     expw0 = exp(w0)
     w1 = w0 - (w0*expw0 - x)/((w0 + 1)*expw0 -
